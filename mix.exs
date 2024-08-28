0db66366bdbe055772013898f50a3c6503ec6837
Benjamin Porter <FreedomBen@users.noreply.github.com>
2024-08-28 14:42:17 -0600

    switch string to int, and bump version 0.2.2

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule CloudflareApi.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/freedomben/cloudflare_api"
-  @version "0.2.1"
+  @version "0.2.2"
 
   def project do
     [
