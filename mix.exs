4e082cd625399d3494809b2197a8bcd09e88a0f0
Benjamin Porter <FreedomBen@users.noreply.github.com>
2024-08-28 14:07:59 -0600

    Bump version to 0.1.0

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule CloudflareApi.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/freedomben/cloudflare_api"
-  @version "0.0.5"
+  @version "0.1.0"
 
   def project do
     [
